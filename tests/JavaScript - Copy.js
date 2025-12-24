8f7f32e8e35ed566b715f25012176342d407716a
awsdevopsonline <punnam.elite@gmail.com>
2025-12-24 12:59:19 +0530

    DEV

---
+++ tests/JavaScript - Copy.js
@@ -0,0 +1,30 @@
+let person ={
+name: "JavaScript",
+year: 1995,
+creator: "Brendan Eich" ,
+age: 40,
+greet: function() {
+ console.log("Hello, I am " + this.name + ", created in " + this.year + " by " + this.creator + ".");   
+
+
+}
+
+
+}
+
+person.greet();
+
+1st-
+person.greet();
+
+A - DEVELOP -1st 
+
+person.greet();
+
+B - DEVELOP -2st 
+
+
+
+person.greet();
+
+B - MASTER -3st 
